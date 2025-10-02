f41f32b4c9393780061ca0a545f0884e9000a955
Yong Hui <65816706+TanYongHui-X@users.noreply.github.com>
2025-10-02 17:28:36 +0800

    test2

--- src/test/java/com/workshop/shopping_carts/ShoppingCartsApplicationTests.java
+++ src/test/java/com/workshop/shopping_carts/ShoppingCartsApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ShoppingCartsApplicationTests {
-//testing git1
+//2testing git2
     @Test
     void contextLoads() {
     }
