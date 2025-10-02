d32bf735dc9a739425b47ef4d85bf6b8e59876f1
zhouq <zhouqian2123@gmail.com>
2025-10-02 12:15:47 +0800

    version1

---
+++ src/test/java/com/workshop/shopping_carts/ShoppingCartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.shopping_carts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
