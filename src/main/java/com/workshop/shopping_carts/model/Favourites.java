cbc51a78c9925f5c753ec6eb42691ae52de18304
zhouq <zhouqian2123@gmail.com>
2025-10-02 14:35:53 +0800

    v1.001 -entity basic 02/10 14:35

---
+++ src/main/java/com/workshop/shopping_carts/model/Favourites.java
@@ -0,0 +1,33 @@
+package com.workshop.shopping_carts.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+
+/**
+ * Favourites Entity Class
+ * Author: Zhou Jayson
+ * Date: 2025-10-02
+ * Modifier by :
+ * Last Modified by :
+ * Last Modified: 2025-10-02 14:00
+ */
+
+@Entity
+@Data
+public class Favourites {
+    /*
+    - productId: int (PK/FK)
+    - customerId: int (PK/FK)
+     */
+    @Id
+    @Setter(AccessLevel.NONE)
+    private int productId;
+    @Id
+    @Setter(AccessLevel.NONE)
+    private int customerId;
+
+}
---
+++ src/main/java/com/workshop/shopping_carts/model/ShoppingCartDetail.java
@@ -0,0 +1,36 @@
+package com.workshop.shopping_carts.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+
+
+/**
+ * ShoppingCartDetail Entity Class
+ * Author: Zhou Jayson
+ * Date: 2025-10-02
+ * Modifier by :
+ * Last Modified by :
+ * Last Modified: 2025-10-02 14:00
+ */
+
+
+@Entity
+@Data
+public class ShoppingCartDetail {
+    /*
+    - productId: int(PK,FK)
+    - customerID: int (PK,FK)
+    - quantity: int
+     */
+    @Id
+    @Setter(AccessLevel.NONE)
+    private int productId;
+    @Id
+    @Setter(AccessLevel.NONE)
+    private int customerID;
+    private int quantity;
+}
