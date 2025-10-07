77e22fbbd67fa15c49b390d932a27ab0ca2e5273
Thae Thae <thaethaehsu23597@gmail.com>
2025-10-07 12:26:45 +0630

    Add files via upload

---
+++ src/main/java/com/Assignment/shopping_carts/InterfaceMethods/ReviewService.java
@@ -0,0 +1,7 @@
+package com.Assignment.shopping_carts.Service;
+
+import com.Assignment.shopping_carts.Model.Review;
+
+public interface ReviewService {
+    public Review saveReview(Review review);
+}
