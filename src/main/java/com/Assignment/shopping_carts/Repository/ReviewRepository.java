12b528b6af905d8939e567e689d71031f921ad1e
Thae Thae <thaethaehsu23597@gmail.com>
2025-10-08 09:18:39 +0630

    Add files via upload

---
+++ src/main/java/com/Assignment/shopping_carts/Repository/ReviewRepository.java
@@ -0,0 +1,24 @@
+package com.Assignment.shopping_carts.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import com.Assignment.shopping_carts.Model.Review;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
+	@Query("SELECT AVG(r.rating) FROM Review r WHERE r.productId = :productId")
+	Double findAverageRatingByProductId(@Param("productId") int productId);
+
+	@Query("SELECT COUNT(r) FROM Review r WHERE r.productId = :productId")
+	Long countReviewsByProductId(@Param("productId") int productId);
+
+	@Query("SELECT r FROM Review r WHERE r.productId = :productId")
+	List<Review> findByProductId(@Param("productId") int productId);
+
+	@Query("SELECT r FROM Review r WHERE r.productId = :productId AND r.customerId = :customerId AND r.orderId = :orderId")
+	Optional<Review> findByProductIdAndCustomerIdAndOrderId(@Param("productId") int productId, @Param("customerId") int customerId, @Param("orderId") int orderId);
+}
