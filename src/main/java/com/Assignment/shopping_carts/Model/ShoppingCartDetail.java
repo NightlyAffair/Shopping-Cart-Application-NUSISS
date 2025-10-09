50e44bb4e7a3b47269dd5454c1cdf043943b87f1
TonySongJQ <songjiaqi0704@gmail.com>
2025-10-09 12:06:32 +0800

    fav html update

--- src/main/java/com/Assignment/shopping_carts/Model/ShoppingCartDetail.java
+++ src/main/java/com/Assignment/shopping_carts/Model/ShoppingCartDetail.java
@@ -13,9 +13,9 @@ import lombok.Setter;
  * ShoppingCartDetail Entity Class
  * Author: Zhou Jayson
  * Date: 2025-10-02
- * Modifier by :
- * Last Modified by :
- * Last Modified: 2025-10-02 14:00
+ * Modifier by : Tony
+ * Last Modified by : Tony
+ * Last Modified: 2025-10-07 23:00
  */
 
 
@@ -35,15 +35,22 @@ public class ShoppingCartDetail {
     private int quantity;
 
     @ManyToOne
+    //分别写明：当前表的FK（customerId），对应customer表的PK（customerId），插入数据时不要重复插入，更新数据时不要重复更新数据
+    //insertable=False意思是插入数据库时，只用customerId字段的值，不要再从customerEntity里取了
+    //updatable=false意思是更新数据库时，也只用customerId字段的值，不要再从customerEntity里取了
     @JoinColumn(name = "customerId", referencedColumnName = "customerId", insertable = false, updatable = false)
     private Customer customer;
     @ManyToOne
+    //分别写明：当前表的FK（productId），对应customer表的PK（productId），插入数据时不要重复插入，更新数据时不要重复更新数据
+    //insertable=False意思是插入数据库时，只用customerId字段的值，不要再从customerEntity里取了
+    //updatable=false意思是更新数据库时，也只用customerId字段的值，不要再从customerEntity里取了
     @JoinColumn(name = "productId", referencedColumnName = "productId", insertable = false, updatable = false)
     private Product product;
 
+    //一个method，计算单种product的总价（单价*数量），method同时考虑有discount的情况
     public double getSubPrice() {
-        //Also considered discount
+        //先get单价，再考虑有discount的情况，计算某个product的最终单价
         double priceAfterDiscount = product.getUnitPrice() * (1 - product.getDiscount());
-        return priceAfterDiscount * quantity;
+        return priceAfterDiscount * quantity;//乘数量获得单种product的总价
     }
 }
