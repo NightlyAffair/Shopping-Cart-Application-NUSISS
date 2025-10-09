9e4b65cc34dce9d6ecd7dbfebbdbc96239d85618
NightlyAffair <nithvinlee@gmail.com>
2025-10-09 12:28:20 +0800

    PLEASE DONT KILL MY BRANCH

---
+++ src/main/java/com/Assignment/shopping_carts/Model/Favourites.java
@@ -0,0 +1,33 @@
+package com.Assignment.shopping_carts.Model;
+
+
+import com.Assignment.shopping_carts.Model.compositeKey.FavouritesId;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Data
+@IdClass(FavouritesId.class)
+public class Favourites {
+        /*
+        - productId: int (PK/FK)
+        - customerId: int (PK/FK)
+         */
+        @Id
+        private int productId;
+
+        @Id
+        @Getter(AccessLevel.NONE)
+        private int customerId;
+
+        @ManyToOne
+        @JoinColumn(name = "productId", referencedColumnName = "productId", insertable = false, updatable = false)
+        private Product product;
+
+        @ManyToOne
+        @JoinColumn(name = "customerId", referencedColumnName = "customerId", insertable = false, updatable = false)
+        private Customer customer;
+}
