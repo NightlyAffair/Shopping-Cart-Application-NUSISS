d4b00cf7df4e925811da9c616c76d4306764e5a1
zhouq <zhouqian2123@gmail.com>
2025-10-09 08:52:01 +0800

    v1.002 -controller, service, interface finished

--- src/main/java/com/Assignment/shopping_carts/Config/WebAppConfig.java
+++ src/main/java/com/Assignment/shopping_carts/Config/WebAppConfig.java
@@ -2,6 +2,7 @@ package com.Assignment.shopping_carts.Config;
 
 import com.Assignment.shopping_carts.Interceptor.LogInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -15,12 +16,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * Modified by: Jason
  * Last Modified: 2025-10-07 11:00
  */
-@Component
+@Configuration
 public class WebAppConfig implements WebMvcConfigurer {
     @Autowired
     LogInterceptor loggingInterceptor;
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(loggingInterceptor).addPathPatterns("/Log");
+        registry.addInterceptor(loggingInterceptor).addPathPatterns("/shoppingCartDetail/**").excludePathPatterns("/Log/**","/api/register/**");
     }
 }
