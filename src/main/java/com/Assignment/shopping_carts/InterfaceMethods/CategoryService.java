d1ba3894d1ca6b0dcb11dc8d2be2e2bae5671147
Glenn Min <113816638+glennmin02@users.noreply.github.com>
2025-10-10 13:44:58 +0800

    Delete src/main directory

--- src/main/java/com/Assignment/shopping_carts/InterfaceMethods/CategoryService.java
+++
@@ -1,9 +0,0 @@
-package com.Assignment.shopping_carts.InterfaceMethods;
-
-import com.Assignment.shopping_carts.Model.Category;
-
-import java.util.List;
-
-public interface CategoryService {
-    List<Category> getCategories();
-}
