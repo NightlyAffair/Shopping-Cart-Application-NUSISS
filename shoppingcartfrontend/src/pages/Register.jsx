00fdbbdb97d7e1a7c4b3cccd40d63c8d4cdc8db0
zhouq <zhouqian2123@gmail.com>
2025-10-11 12:51:19 +0800

    v1.002 -controller, service, interface finished

---
+++ shoppingcartfrontend/src/pages/Register.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Register() {
+    return (
+        <div className="container mt-5">
+            <h2>Register Page</h2>
+            <p>This is the sign-up page.</p>
+            {/* 这里你可以加表单、axios 调用后端接口 */}
+        </div>
+    );
+}
+
+export default Register;
