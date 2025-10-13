7269a8db8277a4e1a75a496c106062f87753dfb5
aung-nusiss <aung.kk1404@gmail.com>
2025-10-13 10:40:10 +0800

    update purchaseHistory

--- src/main/java/com/Assignment/shopping_carts/Repository/OrdersRepository.java
+++ src/main/java/com/Assignment/shopping_carts/Repository/OrdersRepository.java
@@ -3,7 +3,6 @@ package com.Assignment.shopping_carts.Repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
