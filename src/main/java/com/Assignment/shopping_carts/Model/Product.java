f40487ad5d885fe270ff600b09907b5e65d091c5
aung-nusiss <aung.kk1404@gmail.com>
2025-10-13 13:26:02 +0800

    Update OrderDetail and product unidirectional

--- src/main/java/com/Assignment/shopping_carts/Model/Product.java
+++ src/main/java/com/Assignment/shopping_carts/Model/Product.java
@@ -9,18 +9,26 @@
 
 package com.Assignment.shopping_carts.Model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import jakarta.persistence.*;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Setter;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Data
 @Entity
@@ -49,7 +57,7 @@ public class Product {
     @JsonIgnore
     private Category category;
 
-    @OneToMany(mappedBy = "product")
+    @OneToMany
     private List<OrderDetail> orderDetails = new ArrayList<>();
 
 
