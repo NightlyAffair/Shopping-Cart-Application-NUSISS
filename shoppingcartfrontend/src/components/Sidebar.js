d469c153d330de14f26c670f80fd9d722721ddf8
NightlyAffair <nithvinlee@gmail.com>
2025-10-15 17:06:47 +0800

    Added padding to sidebar

--- shoppingcartfrontend/src/components/Sidebar.js
+++ shoppingcartfrontend/src/components/Sidebar.js
@@ -26,13 +26,13 @@ export default function Sidebar() {
     }
 
     return (
-        <div style={{height: "100vh", width: "35vh", backgroundColor: "white", borderRight: '1px solid black', paddingLeft: "25"}} >
+        <div style={{height: "100vh", width: "250px", backgroundColor: "white", borderRight: '1px solid black'}} className="px-3">
             <h4 className="font-weight-bold py-3 mb-4">
                 Account settings
             </h4>
-            <div className="row no-gutters row-bordered row-border-light" style={{width: "30vh"}}>
-                <div className="list col-md-3 pt-0" style={{width: "30vh"}}>
-                        <div className="list-group list-group-flush account-settings-links" style={{width: "29vh"}}>
+            <div className="row no-gutters row-bordered row-border-light">
+                <div className="list col-md-12 pt-0">
+                        <div className="list-group list-group-flush account-settings-links">
                             <a className="list-group-item list-group-item-action" data-toggle="list "
                                onClick={navigateToAccountInfo}>Account Info</a>
                             <a className="list-group-item list-group-item-action" data-toggle="list"
