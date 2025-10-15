4a6a8b4ae30503e57536cfa003a4ccd4fd8a6c97
aung-nusiss <aung.kk1404@gmail.com>
2025-10-15 17:49:38 +0800

    something

--- src/main/java/com/Assignment/shopping_carts/Controller/LogController.java
+++ src/main/java/com/Assignment/shopping_carts/Controller/LogController.java
@@ -1,17 +1,19 @@
 package com.Assignment.shopping_carts.Controller;
 
 
-import com.Assignment.shopping_carts.InterfaceMethods.LogInterface;
-import com.Assignment.shopping_carts.Model.Customer;
-import com.Assignment.shopping_carts.Repository.CustomerRepository;
-import com.Assignment.shopping_carts.Service.LogImpl;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Arrays;
-import java.util.List;
+import com.Assignment.shopping_carts.InterfaceMethods.LogInterface;
+import com.Assignment.shopping_carts.Model.Customer;
+import com.Assignment.shopping_carts.Service.LogImpl;
+
+import jakarta.servlet.http.HttpSession;
 
 /**
  * Log Controller
